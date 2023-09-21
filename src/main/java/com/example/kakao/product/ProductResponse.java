8f3036a12d4a79718e09ca545fb2f84c32ba5e69
bbarkse93 <bbarkse93@gmail.com>
2023-09-21 16:34:08 +0900

    first commit

---
+++ src/main/java/com/example/kakao/product/ProductResponse.java
@@ -0,0 +1,29 @@
+package com.example.kakao.product;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import com.example.kakao.product.option.Option;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+public class ProductResponse {
+
+    // (기능1) 상품 목록보기
+    @ToString
+    @Getter
+    @Setter
+    public static class FindAllDTO {
+
+    }
+
+    // (기능2) 상품 상세보기
+    @Setter
+    public static class FindByIdDTO {
+
+    }
+
+}
---
+++ src/main/java/com/example/kakao/product/ProductService.java
@@ -0,0 +1,32 @@
+package com.example.kakao.product;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.product.option.OptionJPARepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class ProductService {
+
+    private final ProductJPARepository productJPARepository;
+    private final OptionJPARepository optionJPARepository;
+
+    // (기능1) 상품 목록보기
+    public List<ProductResponse.FindAllDTO> findAll(int page) {
+        return null;
+    }
+
+    // (기능2) 상품 상세보기
+    public ProductResponse.FindByIdDTO findById(int id) {
+        return null;
+    }
+
+}
